09ba2dd5b8423f83609607b0cfa4e4bd616f3801
siyeonSon <siyeon0209@naver.com>
2023-04-14 15:32:36 +0900

    initial commit

---
+++ src/main/java/com/opensource/hongroaddong/HongroaddongApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.hongroaddong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongroaddongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongroaddongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/hongroaddong/HongroaddongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.hongroaddong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongroaddongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
