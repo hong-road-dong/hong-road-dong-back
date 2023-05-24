51d8ebd2be5e7343cdc9ef773aa2f0ae6fa98ee0
siyeonSon <siyeon0209@naver.com>
2023-05-25 00:50:43 +0900

    HGD-17 chore(item): add Item:Video 1:1 relation

--- src/main/java/com/opensource/hongroaddong/domain/item/entity/Item.java
+++ src/main/java/com/opensource/hongroaddong/domain/item/entity/Item.java
@@ -3,11 +3,8 @@ package com.opensource.hongroaddong.domain.item.entity;
 import com.opensource.hongroaddong.domain.car.entity.Car;
 import com.opensource.hongroaddong.domain.common.BaseEntity;
 import com.opensource.hongroaddong.domain.member.entity.Member;
-import com.opensource.hongroaddong.domain.timestamp.entity.Timestamp;
+import com.opensource.hongroaddong.domain.video.entity.Video;
 import jakarta.persistence.*;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
@@ -21,8 +18,6 @@ public class Item extends BaseEntity {
     @Column(name = "item_id")
     private Long id;
 
-    private String url;
-
     private String content;
 
     @ManyToOne(fetch = LAZY)
@@ -33,7 +28,8 @@ public class Item extends BaseEntity {
     @JoinColumn(name = "car_id")
     private Car car;
 
-    @OneToMany(mappedBy = "item", fetch = LAZY, cascade = ALL, orphanRemoval = true)
-    private Set<Timestamp> timestamps = new HashSet<>();
+    @OneToOne(fetch = LAZY, cascade = ALL)
+    @JoinColumn(name = "video_id")
+    private Video video;
 
 }
