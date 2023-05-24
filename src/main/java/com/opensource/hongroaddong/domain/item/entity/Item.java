4ce3a40760892624e441b06531a1379f228219b3
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2023-05-25 00:57:04 +0900

    [BE] ERD 수정2 (#8)
    * HGD-17 chore(video): delete Timestamp entity and replace Video entity
    
    * HGD-17 chore(item): add Item:Video 1:1 relation
    
    * HGD-17 chore(member): add drivingDegree and delete manners, sensibility, safety degree
    
    * HGD-17 chore(item): add drivingDegree

--- src/main/java/com/opensource/hongroaddong/domain/item/entity/Item.java
+++ src/main/java/com/opensource/hongroaddong/domain/item/entity/Item.java
@@ -3,11 +3,8 @@ package com.opensource.hongroaddong.domain.item.entity;
 import com.opensource.hongroaddong.domain.car.entity.Car;
 import com.opensource.hongroaddong.domain.common.BaseEntity;
 import com.opensource.hongroaddong.domain.member.entity.Member;
-import com.opensource.hongroaddong.domain.timestamp.entity.Timestamp;
+import com.opensource.hongroaddong.domain.video.entity.Video;
 import jakarta.persistence.*;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
@@ -21,9 +18,9 @@ public class Item extends BaseEntity {
     @Column(name = "item_id")
     private Long id;
 
-    private String url;
-
     private String content;
+
+    private int drivingDegree;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
@@ -33,7 +30,8 @@ public class Item extends BaseEntity {
     @JoinColumn(name = "car_id")
     private Car car;
 
-    @OneToMany(mappedBy = "item", fetch = LAZY, cascade = ALL, orphanRemoval = true)
-    private Set<Timestamp> timestamps = new HashSet<>();
+    @OneToOne(fetch = LAZY, cascade = ALL)
+    @JoinColumn(name = "video_id")
+    private Video video;
 
 }
