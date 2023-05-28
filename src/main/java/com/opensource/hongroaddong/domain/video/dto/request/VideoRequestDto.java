3df62d9e771f727858e11d697237a67e47e0a325
siyeonSon <siyeon0209@naver.com>
2023-05-28 14:31:18 +0900

    chore: request memberId when uploading video

--- src/main/java/com/opensource/hongroaddong/domain/video/dto/request/VideoRequestDto.java
+++ src/main/java/com/opensource/hongroaddong/domain/video/dto/request/VideoRequestDto.java
@@ -1,15 +1,13 @@
 package com.opensource.hongroaddong.domain.video.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +15,9 @@ import java.time.LocalDateTime;
 @Validated
 @ParameterObject
 public class VideoRequestDto {
+        @Schema(description = "사용자 Id", example = "1")
+        private Long memberId;
+
         @Schema(description = "영상 타임 스탬프")
         private LocalDateTime timestamp;
 }
