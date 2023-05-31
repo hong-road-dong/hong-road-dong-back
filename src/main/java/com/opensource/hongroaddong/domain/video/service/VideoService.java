4d7d3ddca3b36a16349f880c9bcbb9b23fc436f1
siyeonSon <siyeon0209@naver.com>
2023-05-31 17:12:54 +0900

    refactor: add Transactional annotation in POST request

--- src/main/java/com/opensource/hongroaddong/domain/video/service/VideoService.java
+++ src/main/java/com/opensource/hongroaddong/domain/video/service/VideoService.java
@@ -6,12 +6,14 @@ import com.opensource.hongroaddong.domain.video.dto.request.VideoRequestDto;
 import com.opensource.hongroaddong.domain.video.dto.response.VideoResponseDto;
 import com.opensource.hongroaddong.domain.video.entity.Video;
 import com.opensource.hongroaddong.domain.video.repository.VideoRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,6 +23,7 @@ public class VideoService {
     private final MemberService memberService;
     private final VideoRepository videoRepository;
 
+    @Transactional
     public VideoResponseDto uploadVideo(VideoRequestDto requestDto, MultipartFile uploadFile) throws IOException {
         var member = memberService.findMember(requestDto.getMemberId());
         var timestamp = requestDto.getTimestamp();
