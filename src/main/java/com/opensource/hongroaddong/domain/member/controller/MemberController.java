cf58d4f4a9850095d4fe002d36d92818346746e2
siyeonSon <siyeon0209@naver.com>
2023-06-01 12:07:31 +0900

    chore: add emoji in end point

--- src/main/java/com/opensource/hongroaddong/domain/member/controller/MemberController.java
+++ src/main/java/com/opensource/hongroaddong/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.opensource.hongroaddong.domain.member.controller;
 import com.opensource.hongroaddong.domain.common.ResponseDto;
 import com.opensource.hongroaddong.domain.member.dto.response.MemberResponseDto;
 import com.opensource.hongroaddong.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
-@Tag(name = "Member", description = "Member API")
+@Tag(name = "🙎Member", description = "Member API")
 public class MemberController {
 
     private final MemberService memberService;
 
     @GetMapping("/{memberId}")
+    @Operation(summary = "내 정보 가져오기")
     public ResponseEntity<MemberResponseDto> getMemberInfo(@Valid @PathVariable Long memberId) {
         var response = memberService.getMemberInfo(memberId);
         return ResponseDto.ok(response);
