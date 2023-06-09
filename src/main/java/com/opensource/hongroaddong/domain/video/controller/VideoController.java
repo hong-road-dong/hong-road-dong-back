1ef38bfd785a504c66d01ab4c86f45032ee21381
siyeonSon <87802191+siyeonSon@users.noreply.github.com>
2023-06-09 18:33:03 +0900

    [BE] 영상 업로드 에러 해결 (#22)
    * fix: add upload-information in request param
    
    * fix: add upload-information in request param per each

--- src/main/java/com/opensource/hongroaddong/domain/video/controller/VideoController.java
+++ src/main/java/com/opensource/hongroaddong/domain/video/controller/VideoController.java
@@ -1,18 +1,18 @@
 package com.opensource.hongroaddong.domain.video.controller;
 
 import com.opensource.hongroaddong.domain.common.ResponseDto;
-import com.opensource.hongroaddong.domain.video.dto.request.VideoUploadRequestDto;
 import com.opensource.hongroaddong.domain.video.dto.response.VideoResponseDto;
 import com.opensource.hongroaddong.domain.video.service.VideoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,13 +23,14 @@ public class VideoController {
 
     private final VideoService videoService;
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "비디오 업로드하기")
     public ResponseEntity<VideoResponseDto> uploadVideo(
-            @Valid @RequestBody VideoUploadRequestDto info,
+            @RequestParam Long memberId,
+            @RequestParam LocalDateTime timestamp,
             @RequestPart(value = "file") MultipartFile multipartFile
     ) throws IOException {
-        var response = videoService.uploadVideo(info, multipartFile);
+        var response = videoService.uploadVideo(memberId, timestamp, multipartFile);
         return ResponseDto.created(response);
     }
 
